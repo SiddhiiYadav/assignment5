e5ff6c9b461402626c8f586d220541c39484d5c8
SiddhiiYadav <136458334+SiddhiiYadav@users.noreply.github.com>
2023-06-19 22:43:15 +0530

    Add files via upload

---
+++ assignment5.py
@@ -0,0 +1,13 @@
+l1=[1,2,3,4,5] 
+print("List: ",l1)
+print("Sum: ",sum(l1))
+print("Minimum value: ",min(l1))
+print("Maximum value: ",max(l1))
+l1.sort()
+print("Ascending: ",l1)
+l1.reverse()
+print("Descending: ",l1)
+l2=(l1)                
+print("Tuple: ",l1)
+del(l2)
+print(l2)
